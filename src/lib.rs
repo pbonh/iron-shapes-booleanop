e40b74911a16c1c6b0bcefc5beccd4dae6d11590
Thomas Kramer <code@tkramer.ch>
2020-11-30 19:43:35 +0100

    Restructure modules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-
 extern crate num_traits;
 extern crate num_rational;
 extern crate itertools;
 
 extern crate iron_shapes;
 
-pub mod boolean;
+mod intersection;
+mod sweep_event;
+mod compare_segments;
+mod connect_edges;
+mod possible_intersection;
+
+/// Type of boolean operation.
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Operation {
+    Intersection,
+    Difference,
+    Union,
+    Xor,
+}
